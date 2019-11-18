22b5c2499c65cf8d4c12344569d71bc7224b447e
kokuma222 <zhaopf123@gmail.com>
2019-11-18 22:03:10 +0800

    java config dispather servlet

---
+++ sam/src/main/java/com/zzz/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.zzz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.zzz.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+        super.configureDefaultServletHandling(configurer);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations(("WEB-INF/statics/css"));
+        registry.addResourceHandler("/js/**").addResourceLocations(("WEB-INF/statics/js"));
+        registry.addResourceHandler("/image/**").addResourceLocations(("WEB-INF/statics/image"));
+        super.addResourceHandlers(registry);
+    }
+}
